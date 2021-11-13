3a258ae49550930907fea440844338802607bd27
Yasir NV <nvyasir@gmail.com>
2021-11-13 15:46:18 +0400

    Update classes_game.py

--- classes_game.py
+++ classes_game.py
@@ -1,3 +1,16 @@
+# Classes
+#
+#
+# You are making a video game! The given code declares a Player class, with its attributes and an intro() method.
+# Complete the code to take the name and level from user input, create a Player object with the corresponding values and call the intro() method of that object.
+#
+# Sample Input
+# Tony
+# 12
+#
+# Sample Output
+# Tony (Level 12)
+
 class Player:
     def __init__(self, name, level):
         self.name = name
