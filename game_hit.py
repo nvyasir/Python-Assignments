1ca4fb82a4027dcc6cf0816f15ee5212f9effd91
nvyasir <nvyasir@gmail.com>
2021-11-14 21:24:42 +0400

    Update game_hit.py

--- game_hit.py
+++ game_hit.py
@@ -1,3 +1,16 @@
+# Shooting Game
+#
+#
+# You are creating a shooting game!
+# The game has two types of enemies, aliens and monsters. You shoot the aliens using your laser, and monsters using your gun.
+# Each hit decreases the lives of the enemies by 1.
+# The given code declares a generic Enemy class, as well as the Alien and Monster classes, with their corresponding lives count.
+# It also defines the hit() method for the Enemy class.
+#
+# You need to do the following to complete the program:
+# 1. Inherit the Alien and Monster classes from the Enemy class.
+# 2. Complete the while loop that continuously takes the weapon of choice from user input and call the corresponding object's hit() method.
+
 class Enemy:
     name = ""
     lives = 0
